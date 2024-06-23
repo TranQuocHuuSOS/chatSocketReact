397936dc4b9f92093cd71c901bb8471face7edb2
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2024-06-23 16:12:58 +0700

    chat app

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from 'path';
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -7,16 +8,20 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-// app.get("/", (req, res) => {
-//   // root route http://localhost:5000/
-//   res.send("hello world!!");
-// });
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"))
+})
+
 
 
 
